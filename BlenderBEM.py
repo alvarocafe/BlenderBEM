e011bbad3429c9c70a4c226650396d86f7f38ecf
alvarocafe <alvaro.campos.ferreira@gmail.com>
2020-11-16 23:53:09 -0300

    Update BlenderBEM.py

--- BlenderBEM.py
+++ BlenderBEM.py
@@ -26,8 +26,8 @@ me = bpy.context.object.data
 uv_layer = me.uv_layers.active.data
 faces = me.polygons
 verts = me.vertices
-coords = []
-elems = []
+coord = []
+elem = []
 for poly in me.polygons:
     print("Polygon index: %d, length: %d" % (poly.index, poly.loop_total))
     print("Vertices in polygon %d:" % poly.index)
@@ -37,17 +37,30 @@ for poly in me.polygons:
         print("    UV: %r" % uv_layer[loop_index].uv)
         print("coords: ")
         print(verts[me.loops[loop_index].vertex_index].co)
-        
-coords = []
-elems = []
+
 for poly in me.polygons:
     print("Polygon index: %d, length: %d" % (poly.index, poly.loop_total))
     print("Vertices in polygon %d:" % poly.index)
     print(list(poly.vertices))
-    elems.append([poly.vertices[0], poly.vertices[1], poly.vertices[2]])
-    
+    elem.append([poly.vertices[0], poly.vertices[1], poly.vertices[2]])
+
 for v in verts:
     print("Vertex: %d" % v.index)
-    coords.append([v.co[0], v.co[1], v.co[2]])
-coords = np.array(coords)
-elems = np.array(elems)
+    coord.append([v.co[0], v.co[1], v.co[2]])
+
+coord = np.array(coord)
+elem = np.array(elem)
+surf = np.array([0,0,1,1,2,2,3,3,4,4])
+surf_bc={0:[0,0],1:[0,1]}
+k = 1
+coord_med, normal, Jac = geometry.node_med(coord, elem)
+
+H, G = index.mount_matrix(coord_med,normal,Jac,coord, elem,k)
+
+# apply boundary condition
+elem_bc = boundary.elem(surf_bc, surf, elem)
+ 
+A, b = index.mount_linear_system(H, G, elem_bc)
+
+x = np.linalg.solve(A, b)
+T, q = index.mount_vector(x, elem_bc)
